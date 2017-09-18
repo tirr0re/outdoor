7126def46c7f11fe38004be8993c0c415b44b068
Your Name <you@example.com>
2017-09-18 15:33:05 +0300

    add footer

--- app/frontend/js/scripts.js
+++ app/frontend/js/scripts.js
@@ -49,6 +49,7 @@ $(document).ready(function () {
 
     });
 
+    // Smooth scroll
     // Select all links with hashes
     $('a[href*="#"]')
     // Remove links that don't actually link to anything
@@ -88,28 +89,44 @@ $(document).ready(function () {
     });
 
     // init Isotope
-    var $grid = $('.portfolio-items').isotope({
-        itemSelector: '.item',
-        layoutMode: 'fitRows'
-    });
-    // bind filter button click
-    $('#filters').on( 'click', 'button', function() {
-        var filterValue = $( this ).attr('data-filter');
-        // use filterFn if matches value
+    $(window).on('load', function(){
+        var $grid = $('.portfolio-items').isotope({
+            itemSelector: '.item',
+            layoutMode: 'fitRows'
+        });
+        // bind filter button click
+        $('#filters').on( 'click', 'button', function() {
+            var filterValue = $( this ).attr('data-filter');
+            // use filterFn if matches value
 
-        $grid.isotope({ filter: filterValue });
-    });
-    // change is-checked class on buttons
-    $('.portfolio-btns').each( function( i, buttonGroup ) {
-        var $buttonGroup = $( buttonGroup );
-        $buttonGroup.on( 'click', 'button', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
+            $grid.isotope({ filter: filterValue });
+        });
+        // change is-checked class on buttons
+        $('.portfolio-btns').each( function( i, buttonGroup ) {
+            var $buttonGroup = $( buttonGroup );
+            $buttonGroup.on( 'click', 'button', function() {
+                $buttonGroup.find('.is-checked').removeClass('is-checked');
+                $( this ).addClass('is-checked');
+            });
         });
     });
 
+
+    // Init Google Map
     // When the window has finished loading create our google map below
     google.maps.event.addDomListener(window, 'load', init);
+
+    var  myLatLng = function() {
+      if (window.innerWidth >= 992) {
+          var myLatLng = {lat: 50.742165, lng: 25.311487};
+          return this;
+      }
+      else if (window.innerWidth >= 768 && window.innerWidth <= 991) {
+          var myLatLng = {lat: 50.742175, lng: 25.311477};
+          return this;
+      }
+    };
+
 
     function init() {
         // Basic options for a simple Google Map
@@ -119,8 +136,7 @@ $(document).ready(function () {
             zoom: 16,
             streetViewControl: false,
             // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(50.742165, 25.311487), // Lutsk
-
+            center: {lat: 50.742165, lng: 25.311487}, // Lutsk
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
             styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"},{"weight":"4"},{"lightness":"-10"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"color":"#dbdbdb"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"color":"#0d0101"},{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cde0fd"},{"visibility":"on"}]}]
@@ -130,9 +146,11 @@ $(document).ready(function () {
         // We are using a div with id="map" seen below in the <body>
         var mapElement = document.getElementById('map');
 
+
+
         // Create the Google Map using our element and options defined above
         var map = new google.maps.Map(mapElement, mapOptions);
-
+        //map.setCenter(new newLatLng(lat, lng));
         // Let's also add a marker while we're at it
         var image = new google.maps.MarkerImage('../images/marker.png',
             new google.maps.Size(127,127),    /* size */
